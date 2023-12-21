b5e7fd73c099266143ec090c9761b8ea68d2a9d7
SheikhRokon <mdrokon.u545@gmail.com>
2023-12-21 13:25:23 +0600

    model create serializers

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    tenure = models.IntegerField()
+    hire_date = models.DateField()
+
