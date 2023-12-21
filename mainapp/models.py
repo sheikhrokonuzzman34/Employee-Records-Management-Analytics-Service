d2c227f4d26c7a5b81fc918dc87c372f32369eaf
SheikhRokon <mdrokon.u545@gmail.com>
2023-12-21 13:47:51 +0600

    up date model

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 class Department(models.Model):
     name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
 
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     tenure = models.IntegerField()
     hire_date = models.DateField()
+    
+    def __str__(self):
+        return self.name
 
