6aba5e2aa2c89d6428559a2281d94987d21812e9
SheikhRokon <mdrokon.u545@gmail.com>
2023-12-21 13:32:47 +0600

    create urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
+    path('departments/', DepartmentListCreateView.as_view(), name='department-list-create'),
+    path('departments/<int:pk>/', DepartmentDetailView.as_view(), name='department-detail'),
+    path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
+    path('employees/<int:pk>/', EmployeeDetailView.as_view(), name='employee-detail'),
+    path('analytics/', EmployeeAnalyticsView.as_view(), name='employee-analytics'),
+    
+    
+    
     path('',home,name='home'),
 ]
